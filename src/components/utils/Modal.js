9ee03d44aed18a90ad9675f96655eb7585298bb8
Hotram89 <marion.dournel@gmail.com>
2023-05-29 18:22:54 +0200

    close modal and navigate to usersList

--- src/components/utils/Modal.js
+++ src/components/utils/Modal.js
@@ -1,12 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import "./Modal.css";
+import React, { useState } from "react";
 
 export default function BasicModal(isOpen) {
+  const [show, setShow] = useState(true);
+  const navigate = useNavigate();
+
+  function handleShow() {
+    setShow(false);
+    navigate("/usersList");
+  }
   return (
     <div id={isOpen ? "modal" : "modal-hidden"}>
-      <div className="modal-content">
-        <span className="close">&times;</span>
-        <p>Employee Created!</p>
-      </div>
+      {show ? (
+        <div className="modal-content">
+          <span className="close" onClick={handleShow}>
+            &times;
+          </span>
+          <p>Employee Created!</p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
